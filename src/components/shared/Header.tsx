3f7221dec81033dbcdae7b902835baa45031e754
rayhanalmim <rayhanalmim1@gmail.com>
2024-11-28 21:47:43 +0600

    Add Redux Toolkit and React-Redux dependencies

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
               type="button"
               className={`py-2.5 px-5 me-2 mb-2 text-sm font-medium rounded-lg border ${
                 activeCategory === category.label
-                  ? "bg-blue-500 text-white" // Active button color
+                  ? "bg-blue-500 text-white" 
                   : "bg-white text-gray-900"
               } focus:outline-none border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700`}
               onClick={() => setActiveCategory(category.label)}
@@ -71,7 +71,7 @@ const Header = () => {
           <div className="bg-white p-6 rounded-lg w-96">
             <AddCategory />
             <button
-              onClick={closeAddCategoryModal} // Close the Add Category Modal
+              onClick={closeAddCategoryModal} 
               className="mt-4 w-full text-white bg-red-600 py-2 rounded-lg"
             >
               Close
@@ -86,7 +86,7 @@ const Header = () => {
           <div className="bg-white p-6 rounded-lg w-96">
             <AddItem />
             <button
-              onClick={closeAddItemModal} // Close the Add Item Modal
+              onClick={closeAddItemModal} 
               className="mt-4 w-full text-white bg-red-600 py-2 rounded-lg"
             >
               Close
