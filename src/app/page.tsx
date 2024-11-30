adb55c62bd83002050e71f8c8717e5b85730e94c
rayhanalmim <rayhanalmim1@gmail.com>
2024-11-30 16:20:27 +0600

    modify existing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import AddItem from "@/components/page/Modal/AddItem";
-import Header from "@/components/shared/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      <Header />
+      <h1>Home</h1>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={72}
+        height={16}
+      />
     </div>
   );
 }
---
+++ src/components/page/connect.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const connect = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default connect;
